3c92f11f230202a972e4daa46e982e8358476bc3
rifathasan <mahihasan83@gmail.com>
2019-07-18 00:07:37 +0600

    New

---
+++ new/src/gradepoint.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class gradepoint {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a =sc.nextInt();
+		Scanner sc1 = new Scanner(System.in);
+		int b =sc.nextInt();
+		
+		if(a>b) {
+			System.out.println("A+");
+			
+		}else {
+			System.out.println("Pass");
+			
+		}
+
+	}
+
+}
